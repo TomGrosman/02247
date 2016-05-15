76b0f1a8300891345247268f8864211ee6ebe3fd
LuisRodriguez95 <luisrodval1995@gmail.com>
2016-05-15 15:37:30 +0200

    Update compareMap.cpp

--- compareMap.cpp
+++ compareMap.cpp
@@ -1,21 +1,64 @@
-void compareMap(std::map<int, ProcessedLine > testMap, CompareType type, int k){ 
-//later we should make a case for each copareType. For the moment I have just implemented the operand case
-  std::map<int,int>::iterator itfin=first.end();
-  std::map<int,int>::iterator itdeb=first.begin();
-  int k =2;
-  int i = itdeb->first;
-  int fin = itfin->first ;
-  while (i != fin){
-    int j = i+k;
-    while (testMap[i].lineOperands != testMap[j].lineOperands && j<fin-k)      j++;
-    int n =1;
-    int cpt=1;
-    while ((testMap[i+n].lineOperands == testMap[j+n].lineOperands) && j<fin){
-      cpt++;
-      n++;
-    }
-    if (cpt >=k)      errs() << cpt << " lines coincidence between the blocks begining at " << i << " and" << j<< endl;
-  }
-  i++;
-}
+void compareMap(std::map<int, ProcessedLine > testMap, CompareType type, int k){
+  switch (type){
+  case operand:
 
+    std::map<int,int>::iterator itfin=first.end();
+    std::map<int,int>::iterator itdeb=first.begin();
+    int k =2;
+    int i = itdeb->first;
+    int fin = itfin->first ;
+    while (i != fin){
+      int j = i+k;
+      while (testMap[i].lineOperands != testMap[j].lineOperands && j<fin-k)      j++;
+      int n =1;
+      int cpt=1;
+      while ((testMap[i+n].lineOperands == testMap[j+n].lineOperands) && j<fin){
+	      cpt++;
+	      n++;
+      }
+      if (cpt >=k)      errs() << cpt << " lines coincidence between the blocks begining at " << i << " and" << j<< endl;
+    }
+    i++;
+    break;
+  case operator:
+
+    std::map<int,int>::iterator itfin=first.end();
+    std::map<int,int>::iterator itdeb=first.begin();
+    int k =2;
+    int i = itdeb->first;
+    int fin = itfin->first ;
+    while (i != fin){
+      int j = i+k;
+      while (testMap[i].lineOperandstors != testMap[j].lineOperators && j<fin-k)      j++;
+      int n =1;
+      int cpt=1;
+      while ((testMap[i+n].lineOperators == testMap[j+n].lineOperators) && j<fin){
+	      cpt++;
+	      n++;
+      }
+      if (cpt >=k)      errs() << cpt << " lines coincidence between the blocks begining at " << i << " and" << j<< endl;
+    }
+    i++;
+    break;
+  case operandsANDoperators:
+
+    std::map<int,int>::iterator itfin=first.end();
+    std::map<int,int>::iterator itdeb=first.begin();
+    int k =2;
+    int i = itdeb->first;
+    int fin = itfin->first ;
+    while (i != fin){
+      int j = i+k;
+      while (testMap[i].lineOperandstors != testMap[j].lineOperators && (testMap[i].lineOperands == testMap[j].lineOperands) && j<fin-k)      j++;
+      int n =1;
+      int cpt=1;
+      while ((testMap[i+n].lineOperators == testMap[j+n].lineOperators) && (testMap[i+n].lineOperands == testMap[j+n].lineOperands) && j<fin){
+	      cpt++;
+	      n++;
+      }
+      if (cpt >=k)      errs() << cpt << " lines coincidence between the blocks begining at " << i << " and" << j << endl;
+    }
+    i++;
+    break;
+  }
+}
