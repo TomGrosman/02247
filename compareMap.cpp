d6985995f0ad512eb243155d8c8c51ab3ad11830
LuisRodriguez95 <luisrodval1995@gmail.com>
2016-05-16 15:04:40 +0200

    Update compareMap.cpp

--- compareMap.cpp
+++ compareMap.cpp
@@ -1,11 +1,10 @@
 void compareMap(std::map<int, ProcessedLine > testMap, CompareType type, int k){
+  std::map<int,int>::iterator itfin=first.end();
+  std::map<int,int>::iterator itdeb=first.begin();
+  int i = itdeb->first;
+  int fin = itfin->first ;
   switch (type){
   case operand:
-
-    std::map<int,int>::iterator itfin=first.end();
-    std::map<int,int>::iterator itdeb=first.begin();
-    int i = itdeb->first;
-    int fin = itfin->first ;
     while (i != fin){
       int j = i+k;
       while (testMap[i].lineOperands != testMap[j].lineOperands && j<fin-k)      j++;
@@ -15,17 +14,11 @@ void compareMap(std::map<int, ProcessedLine > testMap, CompareType type, int k){
 	      cpt++;
 	      n++;
       }
-      if (cpt >=k)      errs() << cpt << " lines coincidence between the blocks begining at " << i << " and" << j<< endl;
+      if (cpt >=k)      errs() << cpt << " lines begining at " << i << " and" << j<< " match with respect to their operands" endl;
     }
     i++;
     break;
   case operator:
-
-    std::map<int,int>::iterator itfin=first.end();
-    std::map<int,int>::iterator itdeb=first.begin();
-    int k =2;
-    int i = itdeb->first;
-    int fin = itfin->first ;
     while (i != fin){
       int j = i+k;
       while (testMap[i].lineOperators != testMap[j].lineOperators && j<fin-k)      j++;
@@ -35,17 +28,11 @@ void compareMap(std::map<int, ProcessedLine > testMap, CompareType type, int k){
 	      cpt++;
 	      n++;
       }
-      if (cpt >=k)      errs() << cpt << " lines coincidence between the blocks begining at " << i << " and" << j<< endl;
+      if (cpt >=k)      errs() << cpt << " lines begining at " << i << " and" << j<< " match with respect to their operators" endl;
     }
     i++;
     break;
   case operandsANDoperators:
-
-    std::map<int,int>::iterator itfin=first.end();
-    std::map<int,int>::iterator itdeb=first.begin();
-    int k =2;
-    int i = itdeb->first;
-    int fin = itfin->first ;
     while (i != fin){
       int j = i+k;
       while (testMap[i].lineOperators != testMap[j].lineOperators && (testMap[i].lineOperands == testMap[j].lineOperands) && j<fin-k)      j++;
@@ -55,34 +42,47 @@ void compareMap(std::map<int, ProcessedLine > testMap, CompareType type, int k){
 	      cpt++;
 	      n++;
       }
-      if (cpt >=k)      errs() << cpt << " lines coincidence between the blocks begining at " << i << " and" << j << endl;
+      if (cpt >=k)      errs() << cpt << " lines begining at " << i << " and" << j<< " match with respect to their operands and operators" endl;
     }
     i++;
     break;
   }
 }
 
-void compareRangesForOperands(int start1, int lenght, int start2, std::map<int, ProcessedLine  > testMap, CompareType type){
+void compareRanges(int start1, int lenght, int start2, std::map<int, ProcessedLine  > testMap, CompareType type){
 switch (type){
 	case operand:
-	float counter = 0.0;
-	for(int i=0; i<lenght; i++)
-	   if(testMap.at(start1+i).lineOperands == testMap.at(start2+i).lineOperands)	      counter++;
-	   errs() << "Ranges of lines are Identical operand wise " << (int)(counter/((float)lenght)*100)<< "%" << "\n";
-	   break;
+	  int end=start1+length;
+	  float counter = 0.0;
+	  int i=0;
+	  while (it<=end){
+	    if(testMap.at(start1).lineOperands == testMap.at(start2).lineOperands)	      counter++;
+	    start1++;
+	    start2++;
+	  }
+	  errs() << "Ranges of lines are Identical operand wise " << (int)(counter/((float)lenght)*100)<< "%" << "\n";
+	  break;
 	case operator:
-	float counter = 0.0;
-	for(int i=0; i<lenght; i++)
-	    if(testMap.at(start1+i).lineOperator == testMap.at(start2+i).lineOperator)     counter++;
-	    errs() << "Ranges of lines are Identical operator wise " << (int)(counter/((float)lenght)*100)<< "%" << "\n";
-	    break;
+	  int end=start1+length;
+	  float counter = 0.0;
+	  int i=0;
+	  while (it<=end){
+	    if(testMap.at(start1).lineOperators == testMap.at(start2).lineOperators)	      counter++;
+	    start1++;
+	    start2++;
+	  }
+	  errs() << "Ranges of lines are Identical operator wise " << (int)(counter/((float)lenght)*100)<< "%" << "\n";
+	  break;
   	case operandsANDoperators:
-	float counter = 0.0;
-	for(int i=0; i<lenght; i++)
-	    if((testMap.at(start1+i).lineOperator == testMap.at(start2+i).lineOperator) && (testMap.at(start1+i).lineOperands == testMap.at(start2+i).lineOperands))
-	       counter++;
-	    errs() << "Ranges of lines are Identical operator and operand wise " << (int)(counter/((float)lenght)*100)<< "%" << "\n";
-	    break;
-	
-}
+	  int end=start1+length;
+	  float counter = 0.0;
+	  int i=0;
+	  while (it<=end){
+	    if((testMap.at(start1).lineOperands == testMap.at(start2).lineOperands) && (testMap.at(start1).lineOperators == testMap.at(start2).lineOperators))	      counter++;
+	    start1++;
+	    start2++;
+	  }
+	  errs() << "Ranges of lines are Identical operator and operand wise " << (int)(counter/((float)lenght)*100)<< "%" << "\n";
+	  break;
+	}
 }
